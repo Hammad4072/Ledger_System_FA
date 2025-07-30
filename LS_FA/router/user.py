3a850a18127f7afcd147835c70f53d1f5f30954d
Abubakarshykh <sheikhabubaker498@gmail.com>
2025-07-30 10:11:42 -0700

    added new file to new branch

--- LS_FA/router/user.py
+++ LS_FA/router/user.py
@@ -1,1 +1,25 @@
-##adding code soon
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from backend import schemas, crud
+from backend.database import SessionLocal
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+get_db_dependency = Depends(get_db)
+
+
+@router.post("/")
+def create_user(
+    user: schemas.UserCreate,
+    db: Session = get_db_dependency,
+):
+    return crud.create_user(db, user)
